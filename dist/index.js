a3978ecc07b8a4e9e75cd4b0a3f86e80d31e9e1c
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-30 01:55:38 +0100

    feat: adding dist

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send({ message: 'Server is up an running' });
+});
+app.get('/api/v1/about', (req, res) => {
+    res.send({
+        email: 'olumuyiwa@gmail.com',
+        current_datetime: new Date().toISOString(),
+        github_url: 'https://github.com/Ng1n3/about-me',
+    });
+});
+app.get('*', (req, res) => {
+    res.status(404).send({ message: 'Route not found' });
+});
+app.listen(PORT, () => {
+    console.log(`Server is currently listening on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
